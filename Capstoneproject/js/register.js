c36299da8772a3dcd491dbc98ce50a4f299f2159
KhaiNguyen1608 <nguyenkhai.huflit@gmail.com>
2022-09-04 22:19:59 +0700

    updated

--- Capstoneproject/js/register.js
+++ Capstoneproject/js/register.js
@@ -21,6 +21,12 @@ let fnCheckRadioButton = (btn) => {
 
 
 let fnValidate = (param,confirmPass) => {
+    document.getElementById("warningEmail").innerHTML = ''
+    document.getElementById("warningPassword").innerHTML = ''
+    document.getElementById("warningPasswordconfirm").innerHTML = ''
+    document.getElementById("warningName").innerHTML = ''
+    document.getElementById("warningPhone").innerHTML = ''
+
     let regNumber = /[^0-9]/g; //Kiểm tra Chỉ có các ký tự số
     let onlyLetter = /^[A-Za-z]+$/;
     let regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -28,31 +34,33 @@ let fnValidate = (param,confirmPass) => {
     let regDate = new RegExp('/^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/');
 
     var tempName = removeVietnameseTones(param.userName)
-
+    let isValid = true;
     if(param.userEmail.trim() === '' || !regEmail.test(param.userEmail)) {
-        document.getElementById("tbEmail").innerHTML = '** Email phải đúng định dạng, không để trống **'
-        document.getElementById("tbEmail").style.display = 'inline-block'
+        document.getElementById("warningEmail").innerHTML = '** Email phải đúng định dạng, không để trống **'
+        // document.getElementById("tbEmail").style.display = 'inline-block'
         isValid = false;
     }
 
     if(param.userPass.trim() === '' || param.userPass.length < 6 || param.userPass.length > 10 ||regPass.test(param.userPass)) {
-        document.getElementById("tbMatKhau").innerHTML = '** Mật Khẩu từ 6-10 ký tự (chứa ít nhất 1 ký tự số, 1 ký tự in hoa, 1 ký tự đặc biệt), không để trống **'
-        document.getElementById("tbMatKhau").style.display = 'inline-block'
+        document.getElementById("warningPassword").innerHTML = '** Mật Khẩu từ 6-10 ký tự (chứa ít nhất 1 ký tự số, 1 ký tự in hoa, 1 ký tự đặc biệt), không để trống **'
+        // document.getElementById("tbMatKhau").style.display = 'inline-block'
         isValid = false;
+    } else if (param.userPass.trim() !== confirmPass.trim()) {
+        document.getElementById("warningPasswordconfirm").innerHTML = '** Mật khẩu xác nhận không giống Mật khẩu **'
     }
 
     if(param.userName.trim() === '' || !onlyLetter.test(tempName)) {
-        document.getElementById("tbTen").innerHTML = '** Tên user phải là chữ, không để trống **'
-        document.getElementById("tbTen").style.display = 'inline-block'
+        document.getElementById("warningName").innerHTML = '** Tên user phải là chữ, không để trống **'
+        // document.getElementById("tbTen").style.display = 'inline-block'
         isValid = false;
     }
   
     if(param.userPhone === '' || regNumber.test(param.userPhone)) {
-        document.getElementById("tbGiolam").innerHTML = '** Số giờ làm trong tháng 80 - 200 giờ, không để trống **'
-        document.getElementById("tbGiolam").style.display = 'inline-block'
+        document.getElementById("warningPhone").innerHTML = '** Chỉ được nhập số **'
+        // document.getElementById("tbGiolam").style.display = 'inline-block'
         isValid = false;
     }
-
+    return isValid;
 }
 
 let fnSubmit = () => {
@@ -64,8 +72,10 @@ let fnSubmit = () => {
     let _isGender = document.getElementById('isMale').checked
 
     let newUser = new myUser(_email, _pass, _name, _phone, _isGender,_passConfirm);
-    let isValid = fnValidate(newUser);
-
+    let isValid = fnValidate(newUser, _passConfirm);
+    if(!isValid) 
+        return;
+        
     var promise = axios({
         url: 'https://shop.cyberlearn.vn/api/Users/signup',
         method:'POST',
@@ -84,7 +94,9 @@ let fnSubmit = () => {
     });
 
     promise.catch(function(err){
-        window.alert(err.response.data.message)
+        let noti = err.response.data.message
+        document.getElementById("warningEmail").innerHTML = noti
+        // window.alert(err.response.data.message)
     })
 }
 
